419eb135dc10df53f81b40d85d9e3f5408929620
AlexBaily <alex.d.baily@gmail.com>
2020-08-14 22:59:28 +0100

    Fix unit test for test_max_price

--- frugal_instances/tests/test_models.py
+++ frugal_instances/tests/test_models.py
@@ -8,7 +8,7 @@ class TestInstanceType(unittest.TestCase):
         instance = InstanceType(name="m4.xlarge", cpu=4, ram_gb=16)
         prices = [0.11, 0.22, 0.33, 0.55, 20]
         for price in prices:
-            instance.add_price(prices)
+            instance.add_price(price)
         max_price = instance.max_price
         self.assertEqual(max_price, 20)
         
