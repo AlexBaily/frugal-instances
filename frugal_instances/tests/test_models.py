85626aa0d3354bc0663ec05bcd4c6464b707683a
AlexBaily <alex.d.baily@gmail.com>
2020-08-17 21:35:31 +0100

    Add tset for calc_metric_price.

--- frugal_instances/tests/test_models.py
+++ frugal_instances/tests/test_models.py
@@ -19,3 +19,12 @@ class TestInstanceType(unittest.TestCase):
             instance.add_price(price)
         instance.calc_avg()
         self.assertEqual(instance.avg_price, 4.242)
+
+    def test_calc_metric_price(self):
+        instance = InstanceType(name="m4.xlarge", cpu=4, ram_gb=16)
+        prices = [0.11, 0.22, 0.33, 0.55, 20]
+        for price in prices:
+            instance.add_price(price)
+        instance.calc_avg()
+        instance.calculate_metric_cost("ram")
+        self.assertEqual(instance.metric_cost, 0.265125)
