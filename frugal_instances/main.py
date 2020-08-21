bf0e1fd00207287115ef13d0b17a6312c61f99a0
AlexBaily <alex.d.baily@gmail.com>
2020-08-21 22:46:27 +0100

    Add to_print argument, defaults to top twenty to JSON

--- frugal_instances/main.py
+++ frugal_instances/main.py
@@ -22,9 +22,10 @@ def parse_args():
         description='Process spot instances and return the cheapest.')
     parser.add_argument('--metric', type=str, default='ram',
             help='Which metric do we want to use, cpu or ram')
+    
     return parser.parse_args()
 
-def main(metric=None):
+def main(metric=None, to_print=None):
     instances = get_instance_types()
     instanceList = list(instances)
     reliabilityJson = get_spot_reliability_data('eu-west-1')
@@ -35,18 +36,25 @@ def main(metric=None):
             instance.chance_of_term = reliabilityJson[instance.name]['r']
         except KeyError:
             logging.info('error: instance type not found' + instance.name)
-    most_expensive = get_most_expensive_instance(instanceList)
-    cheapest_instance = get_cheapest_instance_metric(instanceList)
-    print("name: ", most_expensive.name, " price:", most_expensive.max_price, "\n")
-    print("name: ", cheapest_instance.name, " RAM: ", cheapest_instance.ram_gb, 
+            
+    if to_print == 'most-expensive':
+        most_expensive = get_most_expensive_instance(instanceList)
+        print("name: ", most_expensive.name, " RAM: ", most_expensive.ram_gb, 
+            " avg_price: ", most_expensive.avg_price, 
+            " metric_cost: ", most_expensive.metric_cost)
+    elif to_print == 'cheapest':
+        cheapest_instance = get_cheapest_instance_metric(instanceList)
+        print("name: ", cheapest_instance.name, " RAM: ", cheapest_instance.ram_gb, 
             " avg_price: ", cheapest_instance.avg_price, 
             " metric_cost: ", cheapest_instance.metric_cost)
-    #instanceList.sort(key=lambda x: x.metric_cost, reverse=False)
-    best_instances = sorted((i for i in instanceList if i.chance_of_term == 0),
-                            key=lambda x: x.metric_cost, reverse=False)
-    #Print the top ten cheapest instances based on $ cost per GB of RAM.
-    #print_top_ten(best_instances)
-    print(get_top_twenty_json(best_instances))
+    elif to_print == 'top-ten': 
+        print_top_ten(instanceList)
+    else:
+        best_instances = sorted((i for i in instanceList if i.chance_of_term == 0),
+                                key=lambda x: x.metric_cost, reverse=False)
+        #Print the top ten cheapest instances based on $ cost per GB of RAM.
+        #print_top_ten(best_instances)
+        print(get_top_twenty_json(best_instances))
 
 
 
